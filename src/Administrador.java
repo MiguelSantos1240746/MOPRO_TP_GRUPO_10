ca6f99f8707fafeaa4ac57d8347ce22c34b2a30f
MiguelSantos1240746 <miguelfcp2006@gmail.com>
2025-05-05 21:11:15 +0100

    init commit

---
+++ src/Administrador.java
@@ -0,0 +1,5 @@
+public class Administrador extends Pessoa{
+    public Administrador(String nome, int numeroAluno, String curso, String instituição, String password){
+        super(nome, numeroAluno, curso, instituição, password);
+    }
+}
