be87b35630e130db8f405b72fa99f480704d49d8
MiguelSantos1240746 <miguelfcp2006@gmail.com>
2025-05-13 19:02:17 +0100

    init commit

--- src/Barraca.java
+++ src/Barraca.java
@@ -7,27 +7,18 @@ public class Barraca {
     private String instituição;
     private int quantidadeVenda;
     private double valorTotal;
-    private List <Produto> produtos;
-    private List <Escala> escalas;
+
 
     public Barraca(String nome, String instituição){
         this.nome = nome;
         this.instituição = instituição;
         this.quantidadeVenda = 0;
         this.valorTotal = 0.0;
-        this.produtos = new ArrayList<>();
-        this.escalas = new ArrayList<>();
+
     }
 
 
-    public void adicionarProduto(Produto produto){
-        this.produtos.add(produto);
-    }
 
-
-    public void adicionarEscala(Escala escala){
-        this.escalas.add(escala);
-    }
 
     public String getNome() {
         return nome;
@@ -61,23 +52,5 @@ public class Barraca {
         this.valorTotal = valorTotal;
     }
 
-    public List<Produto> getProdutos() {
-        return produtos;
-    }
 
-    public void setProdutos(List<Produto> produtos) {
-        this.produtos = produtos;
-    }
-
-    public List<Escala> getEscalas() {
-        return escalas;
-    }
-
-    public void setEscalas(List<Escala> escalas) {
-        this.escalas = escalas;
-    }
-
-    public String getClassificação(){
-        return new ClassificadorBarracas().classificar.(this);
-    }
 }
